18d9d62c3758bac3ae0b6f4c63428cc750e7e2ba
subham-c <pc562835@gmail.com>
2025-12-19 20:40:11 +0530

    new shop data

---
+++ shop-data/app.py
@@ -0,0 +1,119 @@
+import csv
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+#import mysql.connector
+#from mysql.connector import Error
+#from datetime import datetime
+import os
+import utils # Written by us
+#from utils import DATA_FILE #name of the data file in csv format
+DATA_FILE = 'phone_book2.csv'  # ← Change this to use a different table
+PORT = 5050
+
+app = Flask(__name__)
+CORS(app)
+
+# Table name configuration
+CSV_FILE = DATA_FILE
+# Initialize CSV file with headers if it doesn't exist
+def init_csv(CSV_FILE=CSV_FILE):
+    if not os.path.exists(CSV_FILE):
+        with open(CSV_FILE, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['name', 'phone', 'email','class','school'])  # Add more headers as needed
+        print(f"Initialized new CSV file: {CSV_FILE}")
+        #close the file
+        f.close()
+    else:
+        print(f"CSV file already exists: {CSV_FILE}")
+
+@app.route('/submit', methods=['POST'])
+def submit_data():
+    try:
+        data = request.get_json()
+        print(f"\nReceived data for entry: {data}")
+        for key, value in data.items():
+            data[key] = value.strip()
+            if key == 'dataFile':
+                global CSV_FILE
+                CSV_FILE = value.strip()
+            #print(f"Key: {key}, Value: {value.strip()}")
+            #print list values
+        #print(list(data.values()))
+        #print(f"Received data: {data}") 
+        name = data.get('name', '').strip()
+        phone = data.get('phone', '').strip()
+        email = data.get('email', '').strip()
+        #timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if not name:
+            return jsonify({'success': False, 'message': 'Name is required'}), 400
+        if (not phone) and (not email):
+            return jsonify({
+                'success': False,
+                'message': 'phone or email are required Bhai!'
+            }), 400
+        print(f"Writing data to CSV file: {CSV_FILE}")
+        # Append data to CSV file
+        with open(CSV_FILE, 'a', newline='') as f:
+            writer = csv.writer(f)
+            if 'dataFile' in data:
+                #Delete dataFile from data to avoid writing it to CSV
+                del data['dataFile']
+            writer.writerow(data.values())
+            print(f"\nAdded data to CSV file {CSV_FILE}: {list(data.values())}")
+        return jsonify({
+            'success': True,
+            'message': 'Data saved successfully'
+        }), 201
+    except Exception as e:
+        return jsonify({'success': False, 'message': f'Error: {str(e)}'}), 500
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'}), 200
+
+@app.route('/search', methods=['POST'])
+def search_csv():
+    print(f"Received POST search") 
+    data = request.get_json()
+    print(f"Received data for search: {data}")
+    for key, value in data.items():
+            data[key] = value.strip()
+            if key == 'dataFile':
+                global CSV_FILE
+                CSV_FILE = value.strip()
+            elif key == 'name':
+                name = value.strip().lower()
+            elif key == 'phone':
+                phone = value.strip()
+            elif key == 'email':
+                email = value.strip().lower()             # Assuming email is case-insensitive
+    print(f"Search term: {name} or {phone} or {email}")
+    if (not phone) and (not email) and (not name):
+        return jsonify({
+        'success': False,
+        'message': 'Name or phone or email is required Bhai!'
+        }), 400
+    if name:
+         search_term = name
+    elif phone:
+         search_term = phone
+    else:
+         search_term = email
+    results = utils.search_contact(CSV_FILE, search_term)
+    if results:
+        print(f"Sending search results: {results}") 
+        return jsonify({
+        'success': True,
+        'message': results
+        }), 201
+    else:
+        return jsonify({
+            'success': False,
+            'message': f'No details found for {name} in file: {CSV_FILE}.'
+        }), 404
+    
+if __name__ == '__main__':
+    #init_csv()
+    #app.run(debug=True, port=5050)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
