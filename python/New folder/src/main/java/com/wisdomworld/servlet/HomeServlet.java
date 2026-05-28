62acfccd5482dcc5abe2155a7cb58eaf58a7a890
subham-c <pc562835@gmail.com>
2026-05-28 16:41:02 +0530

    Add school website with gallery, documents, and updated contact information

---
+++ python/New folder/src/main/java/com/wisdomworld/servlet/HomeServlet.java
@@ -0,0 +1,21 @@
+package com.wisdomworld.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet for handling home page requests
+ */
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("index.html");
+    }
+}
